07f296b6660452daab8e331c796387fc390e716a
debuggingbytes <94572956+debuggingbytes@users.noreply.github.com>
2025-02-16 08:33:17 -0700

    Coderabbit is an idiot, public array breaks with collections

--- app/View/Components/layout/Intro.php
+++ app/View/Components/layout/Intro.php
@@ -11,7 +11,7 @@ class Intro extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct(public array $types)
+    public function __construct(public mixed $types)
     {
     }
 
